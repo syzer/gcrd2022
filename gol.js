82fac61381ca2e170b9a50884259633c54277f6e
Hexagon Six <hexagon6@fet.li>
2022-11-05 14:17:17 +0100

    wip

--- gol.js
+++ gol.js
@@ -3,3 +3,7 @@ export const empty = ""
 export const next = () => ""
 
 export const singleCell = !!empty
+
+export const countAliveCells = () => undefined
+
+export const twoCells = () => undefined
