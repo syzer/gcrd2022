62243e73bde63524c1488881d121fdaeb119afd8
Lukasz Gintowt <syzer3@gmail.com>
2022-11-05 14:09:55 +0100

    fix test

--- gol.test.js
+++ gol.test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { empty, next } from './gol'
+import { empty, next } from './gol.js'
 
 test('empty world is empty', t => {
   t.deepEqual(next(empty), empty)
@@ -7,4 +7,7 @@ test('empty world is empty', t => {
   t.deepEqual(next(next(empty)), next(empty))
 })
 
+test('single cell dies', t => {
+  t.deepEqual(next(singleCell), empty)
+})
 /* eslint-ignore */
