f2dd5b5c0f2214b3c00699bcb9e44e4da15d38d5
Lukasz Gintowt <syzer3@gmail.com>
2022-11-05 14:14:49 +0100

    enjoy

--- gol.test.js
+++ gol.test.js
@@ -1,5 +1,5 @@
 import test from "ava"
-import { empty, next, singleCell } from "./gol.js"
+import { empty, next, singleCell, countAliveCells } from "./gol.js"
 
 test("empty world is empty", (t) => {
   t.deepEqual(next(empty), empty)
@@ -10,4 +10,11 @@ test("empty world is empty", (t) => {
 test("single cell dies", (t) => {
   t.deepEqual(next(singleCell), empty)
   t.notDeepEqual(next(empty), singleCell)
+})
+
+test("how many we got", (t) => {
+  t.deepEqual(countAliveCells(empty, 0))
+  t.deepEqual(countAliveCells(singleCell, 1))
+  t.deepEqual(countAliveCells(next(singleCell), 0))
+  t.deepEqual(countAliveCells(next(empty), 0))
 })
