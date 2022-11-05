ab2cdd433c637e4c530cf48d9e4be864cdbb1663
Lukasz Gintowt <syzer3@gmail.com>
2022-11-05 14:59:00 +0100

    initial

--- gol.js
+++
@@ -1,9 +0,0 @@
-export const empty = ""
-
-export const next = () => ""
-
-export const singleCell = !!empty
-
-export const countAliveCells = () => undefined
-
-export const twoCells = () => undefined
