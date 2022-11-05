25afc06215d0a72664d3525bf7d207c3408e914d
Lukasz Gintowt <syzer3@gmail.com>
2022-11-05 14:09:10 +0100

    fix test

--- gol.test.js
+++ gol.test.js
@@ -1,8 +1,10 @@
-import test from "ava";
-import { empty, next } from "./gol.js";
+import test from 'ava';
+import { empty, next } from './gol'
 
-test("empty world is emptu", (t) => {
-  t.deepEqual(next(empty), empty);
-});
+test('empty world is empty', t => {
+  t.deepEqual(next(empty), empty)
+  t.deepEqual(next(next(empty)), empty)
+  t.deepEqual(next(next(empty)), next(empty))
+})
 
 /* eslint-ignore */
