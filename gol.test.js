8d9f129dbcbf7972d00d32c7d00f0d6848b94a0b
Hexagon Six <hexagon6@fet.li>
2022-11-05 14:07:59 +0100

    wip

--- gol.test.js
+++ gol.test.js
@@ -1,6 +1,8 @@
-import test from 'ava';
-import {empty, next} from './gol'
+import test from "ava";
+import { empty, next } from "./gol.js";
 
-test('empty world is emptu', t => {
-  t.deepEqual(next(empty), empty)
+test("empty world is emptu", (t) => {
+  t.deepEqual(next(empty), empty);
 });
+
+/* eslint-ignore */
